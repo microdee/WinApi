e0508e6988fc5a83f7b4bf7cf88c814afa7e4dc1
microdee <m@mcro.de>
2020-01-15 23:00:05 +0100

    added WinApiHelpers for very basic helper functions

---
+++ WinApi/Core/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinApi.Core
+{
+    public static class WinApiHelpers
+    {
+        public static uint BgrColor(byte r, byte g, byte b)
+        {
+            return (uint)b | ((uint)g << 8) | ((uint)r << 16);
+        }
+        public static uint BgraColor(byte r, byte g, byte b, byte a)
+        {
+            return (uint)b | ((uint)g << 8) | ((uint)r << 16) | ((uint)a << 24);
+        }
+    }
+}
